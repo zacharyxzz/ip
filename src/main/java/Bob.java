3859f5b54f99fee18c08c982be91404e10a25702
zacharyxzz <xiebawang@hotmail.com>
2024-08-23 18:43:24 +0800

    level1

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -10,11 +10,20 @@ public class Bob {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
+        while (true) {
+            input = scanner.nextLine();
 
+            if (input.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
-
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(input);
+                System.out.println("____________________________________________________________");
+            }
+        }
 
         scanner.close();
     }
