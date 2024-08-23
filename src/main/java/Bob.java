5e946360a6815225956626108f88fe3363079a64
zacharyxzz <xiebawang@hotmail.com>
2024-08-23 18:32:09 +0800

    level0

---
+++ src/main/java/Bob.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Bob");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+
+
+        scanner.close();
+    }
+}
