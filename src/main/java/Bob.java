80ee181d51f218a2939429e55dc59e4bd15ec935
zacharyxzz <xiebawang@hotmail.com>
2024-08-28 23:29:59 +0800

    level2

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String input;
 
         System.out.println("____________________________________________________________");
@@ -18,10 +20,28 @@ public class Bob {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                if (taskCount == 0) {
+                    System.out.println("No tasks added yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
             } else {
-                System.out.println("____________________________________________________________");
-                System.out.println(input);
-                System.out.println("____________________________________________________________");
+                if (taskCount < 100) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + input);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Task list is full. Cannot add more tasks.");
+                    System.out.println("____________________________________________________________");
+                }
             }
         }
 
