04d6065cfc7e77521a57bf39ff2f8a07abce7d74
zacharyxzz <xiebawang@hotmail.com>
2024-08-28 23:57:28 +0800

    level6

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,10 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCount = 0;
+        ArrayList<Task> tasks = new ArrayList<>();
         String input;
 
         System.out.println("____________________________________________________________");
@@ -23,29 +23,19 @@ public class Bob {
                     System.out.println("____________________________________________________________");
                     break;
                 } else if (input.equals("list")) {
-                    System.out.println("____________________________________________________________");
-                    if (taskCount == 0) {
-                        System.out.println("No tasks added yet.");
-                    } else {
-                        System.out.println("Here are the tasks in your list:");
-                        for (int i = 0; i < taskCount; i++) {
-                            System.out.println((i + 1) + "." + tasks[i]);
-                        }
-                    }
-                    System.out.println("____________________________________________________________");
+                    listTasks(tasks);
                 } else if (inputSplit[0].equals("mark")) {
-                    handleMarkCommand(inputSplit, tasks, taskCount);
+                    handleMarkCommand(inputSplit, tasks);
                 } else if (inputSplit[0].equals("unmark")) {
-                    handleUnmarkCommand(inputSplit, tasks, taskCount);
+                    handleUnmarkCommand(inputSplit, tasks);
                 } else if (inputSplit[0].equals("todo")) {
-                    handleTodoCommand(inputSplit, tasks, taskCount);
-                    taskCount++;
+                    handleTodoCommand(inputSplit, tasks);
                 } else if (inputSplit[0].equals("deadline")) {
-                    handleDeadlineCommand(inputSplit, tasks, taskCount);
-                    taskCount++;
+                    handleDeadlineCommand(inputSplit, tasks);
                 } else if (inputSplit[0].equals("event")) {
-                    handleEventCommand(inputSplit, tasks, taskCount);
-                    taskCount++;
+                    handleEventCommand(inputSplit, tasks);
+                } else if (inputSplit[0].equals("delete")) {
+                    handleDeleteCommand(inputSplit, tasks);
                 } else {
                     throw new BobException("I'm sorry, but I don't know what that means :-(");
                 }
@@ -59,76 +49,97 @@ public class Bob {
         scanner.close();
     }
 
-    private static void handleMarkCommand(String[] inputSplit, Task[] tasks, int taskCount) throws BobException {
-        if (inputSplit.length < 2 || !isValidNumber(inputSplit[1], taskCount)) {
-            throw new BobException("Invalid task number.");
+    private static void listTasks(ArrayList<Task> tasks) {
+        System.out.println("____________________________________________________________");
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks added yet.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + "." + tasks.get(i));
+            }
         }
-        int index = Integer.parseInt(inputSplit[1]) - 1;
-        tasks[index].markAsDone();
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void handleMarkCommand(String[] inputSplit, ArrayList<Task> tasks) throws BobException {
+        int index = parseIndex(inputSplit[1], tasks.size());
+        tasks.get(index).markAsDone();
         System.out.println("____________________________________________________________");
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + tasks[index]);
+        System.out.println("  " + tasks.get(index));
         System.out.println("____________________________________________________________");
     }
 
-    private static void handleUnmarkCommand(String[] inputSplit, Task[] tasks, int taskCount) throws BobException {
-        if (inputSplit.length < 2 || !isValidNumber(inputSplit[1], taskCount)) {
-            throw new BobException("Invalid task number.");
-        }
-        int index = Integer.parseInt(inputSplit[1]) - 1;
-        tasks[index].unmarkAsDone();
+    private static void handleUnmarkCommand(String[] inputSplit, ArrayList<Task> tasks) throws BobException {
+        int index = parseIndex(inputSplit[1], tasks.size());
+        tasks.get(index).unmarkAsDone();
         System.out.println("____________________________________________________________");
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  " + tasks[index]);
+        System.out.println("  " + tasks.get(index));
         System.out.println("____________________________________________________________");
     }
 
-    private static void handleTodoCommand(String[] inputSplit, Task[] tasks, int taskCount) throws BobException {
+    private static void handleTodoCommand(String[] inputSplit, ArrayList<Task> tasks) throws BobException {
         if (inputSplit.length < 2 || inputSplit[1].trim().isEmpty()) {
             throw new BobException("The description of a todo cannot be empty.");
         }
-        tasks[taskCount] = new Todo(inputSplit[1]);
+        tasks.add(new Todo(inputSplit[1]));
         System.out.println("____________________________________________________________");
         System.out.println("Got it. I've added this task:");
         System.out.println("  [T][ ] " + inputSplit[1]);
-        System.out.println("Now you have " + (taskCount + 1) + " tasks in the list.");
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
         System.out.println("____________________________________________________________");
     }
 
-    private static void handleDeadlineCommand(String[] inputSplit, Task[] tasks, int taskCount) throws BobException {
+    private static void handleDeadlineCommand(String[] inputSplit, ArrayList<Task> tasks) throws BobException {
         if (inputSplit.length < 2 || !inputSplit[1].contains(" /by ")) {
             throw new BobException("The description of a deadline must include '/by' followed by a date/time.");
         }
         String[] parts = inputSplit[1].split(" /by ", 2);
-        tasks[taskCount] = new Deadline(parts[0], parts[1]);
+        tasks.add(new Deadline(parts[0], parts[1]));
         System.out.println("____________________________________________________________");
         System.out.println("Got it. I've added this task:");
         System.out.println("  [D][ ] " + parts[0] + " (by: " + parts[1] + ")");
-        System.out.println("Now you have " + (taskCount + 1) + " tasks in the list.");
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
         System.out.println("____________________________________________________________");
     }
 
-    private static void handleEventCommand(String[] inputSplit, Task[] tasks, int taskCount) throws BobException {
+    private static void handleEventCommand(String[] inputSplit, ArrayList<Task> tasks) throws BobException {
         if (inputSplit.length < 2 || !inputSplit[1].contains(" /from ") || !inputSplit[1].contains(" /to ")) {
             throw new BobException("The description of an event must include '/from' followed by start time and '/to' followed by end time.");
         }
         String[] parts = inputSplit[1].split(" /from ", 2);
         String[] fromTo = parts[1].split(" /to ", 2);
-        tasks[taskCount] = new Event(parts[0], fromTo[0], fromTo[1]);
+        tasks.add(new Event(parts[0], fromTo[0], fromTo[1]));
         System.out.println("____________________________________________________________");
         System.out.println("Got it. I've added this task:");
         System.out.println("  [E][ ] " + parts[0] + " (from: " + fromTo[0] + " to: " + fromTo[1] + ")");
-        System.out.println("Now you have " + (taskCount + 1) + " tasks in the list.");
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
         System.out.println("____________________________________________________________");
     }
 
-    private static boolean isValidNumber(String input, int taskCount) {
+    private static void handleDeleteCommand(String[] inputSplit, ArrayList<Task> tasks) throws BobException {
+        int index = parseIndex(inputSplit[1], tasks.size());
+        Task removedTask = tasks.remove(index);
+        System.out.println("____________________________________________________________");
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("  " + removedTask);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static int parseIndex(String input, int size) throws BobException {
         try {
-            int number = Integer.parseInt(input);
-            return number > 0 && number <= taskCount;
+            int index = Integer.parseInt(input) - 1;
+            if (index < 0 || index >= size) {
+                throw new BobException("Invalid task number.");
+            }
+            return index;
         } catch (NumberFormatException e) {
-            return false;
+            throw new BobException("Invalid task number.");
         }
     }
 }
+
 
